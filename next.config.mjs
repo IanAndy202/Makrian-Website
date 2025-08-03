30add0f03aaa9fc7ccbceec1847167168d1cc509
ianandy <ianalden02@gmail.com>
2025-08-03 09:16:49 +0300

    2nd change to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // ✅ Add this line
+  images: {
+    unoptimized: true, // ✅ REQUIRED for <Image> to work in static export
+  },
 };
 
 export default nextConfig;
