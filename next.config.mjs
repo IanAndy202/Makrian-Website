ae03ed7b7ed373e04ee7efa4c4b5a1bf3ac95480
ianandy <ianalden02@gmail.com>
2025-08-05 14:37:13 +0300

    Deploy updated static site fr

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // ✅ Add this line
-  images: {
-    unoptimized: true, // ✅ REQUIRED for <Image> to work in static export
-  },
-};
-
-export default nextConfig;
